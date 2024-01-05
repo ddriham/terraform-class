5ec2fa58c42ee22820447e1f7e62d186e89be9ae
David Driham <david.driham@gmail.com>
2024-01-05 18:45:03 +0200

    add terraform syntax

---
+++ terraform-syntax/Builtin_functions.tf
@@ -0,0 +1,39 @@
+MAX
+
+    >max(11,56,5,55)
+    56
+
+
+
+Join
+    > join("_",["app","front","instance"])
+    app_front_instance
+
+Hash
+
+    md5("Hello World")
+    return Hash....
+
+
+Timestamp
+
+    timestamp()
+
+    2023-12-20T19:05:00
+
+
+Split
+
+    split("_",["app_front_instance"])
+        ["app","front","instance"]
+
+
+
+concat
+
+
+    concat(["a","b"],["c","d"])
+    ["a","b","c","d"]
+
+
+    
